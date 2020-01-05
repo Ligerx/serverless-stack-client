1be7b0ad5518028bb26cab4f299e571b7c3bbb4a
Alex Wang <ipoopfool@gmail.com>
2020-01-05 14:39:09 -0800

    set up aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "ligerx-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://wk711kn1xd.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_xVqu88iUL",
+      APP_CLIENT_ID: "2u5qlaomv82g6c1q44oleholi7",
+      IDENTITY_POOL_ID: "us-west-2:88486173-ee98-41e1-a235-847f72473c9f"
+    }
+};
