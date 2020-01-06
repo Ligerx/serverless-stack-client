6781752a29663866df6cba5aa5f90eee98c54f61
Alex Wang <ipoopfool@gmail.com>
2020-01-05 16:05:28 -0800

    add new note

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
 import AppliedRoute from "./components/AppliedRoute";
 
 export default function Routes({ appProps }) {
@@ -10,6 +12,9 @@ export default function Routes({ appProps }) {
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- src/config.js
+++ src/config.js
@@ -12,5 +12,6 @@ export default {
       USER_POOL_ID: "us-west-2_xVqu88iUL",
       APP_CLIENT_ID: "2u5qlaomv82g6c1q44oleholi7",
       IDENTITY_POOL_ID: "us-west-2:88486173-ee98-41e1-a235-847f72473c9f"
-    }
+    },
+    MAX_ATTACHMENT_SIZE: 5000000,
 };
